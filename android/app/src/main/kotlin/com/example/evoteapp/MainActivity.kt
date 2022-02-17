4e8eb024df232ae776e328786b6c9b91db0a8f39
denrix0 <aby2800@gmail.com>
2022-02-17 11:15:41 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/evoteapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.evoteapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthenticatePage(),
+    );
+  }
+}
+
+class AuthenticatePage extends StatefulWidget {
+  const AuthenticatePage({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticatePageState createState() => _AuthenticatePageState();
+}
+
+class _AuthenticatePageState extends State<AuthenticatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
